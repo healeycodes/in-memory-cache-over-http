4f37be24505ddbfacd323cb43c66965083a6280b
Andrew <healeycodes@gmail.com>
2019-10-12 23:04:03 +0100

    Rework API to use strings everywhere

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func setup() {
 	http.HandleFunc("/set", Set)
 }
 
-// Listen on PORT. Defaults to 8000
+// Listen on PORT. Defaults to 8000.
 func start() {
 	err := http.ListenAndServe(getEnv("PORT", ":8000"), nil)
 	if err != nil {
@@ -29,7 +29,8 @@ func start() {
 	}
 }
 
-// Get a key from the store. Status code: 200 if present, else 404.
+// Get a key from the store.
+// Status code: 200 if present, else 404.
 // e.g. ?key=foo
 func Get(w http.ResponseWriter, r *http.Request) {
 	value, exist := s.Get(r.URL.Query().Get("key"))
@@ -39,15 +40,37 @@ func Get(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("content-type", "text/plain")
-	w.Write([]byte(value.(string)))
+	w.Write([]byte(value))
 }
 
-// Set a key in the store. Status code: 204.
+// Set a key in the store.
+// Status code: 204.
 func Set(w http.ResponseWriter, r *http.Request) {
 	s.Set(r.URL.Query().Get("key"), r.URL.Query().Get("value"))
 	w.WriteHeader(http.StatusNoContent)
 }
 
+// Increment a key in the store. Sets 1 if new key.
+// Status code: 204 if incrementable else 400.
+func Increment(w http.ResponseWriter, r *http.Request) {
+	if err := s.Increment(r.URL.Query().Get("key")); err == nil {
+		w.WriteHeader(http.StatusNoContent)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+// Decrement a key in the store. Sets -1 if new key.
+// Status code: 204 if decrementable else 400.
+func Decrement(w http.ResponseWriter, r *http.Request) {
+	if err := s.Decrement(r.URL.Query().Get("key")); err == nil {
+		w.WriteHeader(http.StatusNoContent)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+// Gets an ENV variable else returns fallback.
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
--- store/store.go
+++ store/store.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"strconv"
 	"sync"
 )
 
@@ -16,18 +17,49 @@ func StoreService() Store {
 }
 
 // Set a key
-func (s Store) Set(key string, value interface{}) {
+func (s Store) Set(key string, value string) {
 	s.keys.Store(key, value)
 }
 
 // Get a key
-func (s Store) Get(key string) (interface{}, bool) {
-	return s.keys.Load(key)
+func (s Store) Get(key string) (string, bool) {
+	val, exist := s.keys.Load(key)
+	return string(val.(string)), exist
 }
 
 // Delete a key
 func (s Store) Delete(key string) {
 	s.keys.Delete(key)
+}
+
+// Increment a key. If doesn't exist, increment from zero.
+func (s Store) Increment(key string) error {
+	val, exist := s.Get(key)
+	if !exist {
+		s.Set(key, "1")
+		return nil
+	}
+	if n, err := strconv.Atoi(val); err == nil {
+		s.Set(key, strconv.Itoa(n+1))
+		return nil
+	} else {
+		return err
+	}
+}
+
+// Decrement a key. If doesn't exist, decrement from zero.
+func (s Store) Decrement(key string) error {
+	val, exist := s.Get(key)
+	if !exist {
+		s.Set(key, "-1")
+		return nil
+	}
+	if n, err := strconv.Atoi(val); err == nil {
+		s.Set(key, strconv.Itoa(n-1))
+		return nil
+	} else {
+		return err
+	}
 }
 
 // Delete all keys
