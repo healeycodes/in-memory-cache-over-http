ed4dd5533fcd089f50054fc18be74aee355eab9b
Andrew <healeycodes@gmail.com>
2019-10-12 15:28:54 +0100

    Continue testing package structure

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 	store "github.com/healeycodes/key-value-store/store"
 )
 
-keys := new(sync.Map)
 func main() {
+	keys := new(sync.Map)
 	store.Set("Test", 1)
 	fmt.Println("Test")
 }
--- store/store.go
+++ store/store.go
@@ -1,7 +1,6 @@
 package store
 
 import (
-	"fmt"
 	"sync"
 )
 
